2a9c401c88ac6380efba34d4479fb1d5c3f9c4c2
Justus <justus1274@gmail.com>
2025-06-15 16:33:00 -0700

    fixing

--- src/pages/[...alll].tsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-// any unmatched route will land here
-export default function CatchAll() {
-  // redirect back to "/"
-  return <Navigate to="/" replace />;
-}
