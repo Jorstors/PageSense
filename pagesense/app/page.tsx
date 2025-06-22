6fe30d6d7d89f9bc2f7129c7007717ecefb388b0
Justus <justus1274@gmail.com>
2025-06-21 17:14:09 -0700

    Migrated to NextJS, translated framework.

---
+++ pagesense/app/page.tsx
@@ -0,0 +1,15 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+export default function Main() {
+  return (
+    <div className="dark w-screen min-h-screen bg-background text-primary ">
+      <div className="w-screen h-screen flex">
+        <ScrollArea type="always" className="w-full h-full">
+          <div className="h-screen w-full flex flex-col items-center justify-center">
+            Landing Page
+          </div>
+        </ScrollArea>
+      </div>
+    </div>
+  );
+}
---
+++ pagesense/next.config.ts
@@ -0,0 +1,25 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  productionBrowserSourceMaps: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: new URL(process.env.NEXT_PUBLIC_URL).hostname,
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+    ],
+    deviceSizes: [640, 750, 828, 1080, 1200],
+    imageSizes: [16, 32, 48, 64, 96],
+  }
+};
+
+export default nextConfig;
