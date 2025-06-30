3011b8d2d63ebb55632c1661b965c2a1a75e2cb7
Justus Jones <justus1274@gmail.com>
2025-06-29 18:59:26 -0700

    Adds animated blur-fade effect to construction header

--- components/Construction.tsx
+++ components/Construction.tsx
@@ -1,17 +1,24 @@
 import React from "react";
-import { ChevronRightIcon } from "lucide-react";
+import { ChevronRightIcon, ConstructionIcon } from "lucide-react";
 import { TypingAnimation } from "./magicui/typing-animation";
+import { BlurFade } from "./magicui/blur-fade";
 import { TextEffect } from "./motion-primitives/text-effect";
 import { Button } from "./ui/button";
 import Link from "next/link";
 
 const Construction: React.FC = () => {
   return (
-    <div className="text-center p-12 w-lg lg:w-3xl">
-      <div className="flex items-center justify-around gap-5 w-full">
+    <div className="text-center p-12 w-lg lg:w-3xl text-xs md:text-base grid place-items-center">
+      <div className="inline-flex gap-5 w-fit">
+        <BlurFade delay={2} duration={1}>
+          <ConstructionIcon size={60} />
+        </BlurFade>
         <TypingAnimation as="h1" delay={0}>
-          🚧Under Construction🚧
+          Under Construction
         </TypingAnimation>
+        <BlurFade delay={2} duration={1}>
+          <ConstructionIcon size={60} />
+        </BlurFade>
       </div>
       <TextEffect per="char" delay={3} className="my-5">
         We&apos;re working hard to bring this page to life. Please check back
