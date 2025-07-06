9455c03f972eab48daf855cc1a079b5baf3516e6
Justus <justus1274@gmail.com>
2025-07-05 20:34:34 -0700

    Enhance form submission handling in Login and Signup components with timeout for re-enabling submission

--- components/signup.tsx
+++ components/signup.tsx
@@ -74,14 +74,17 @@ const Signup = ({
     console.log(data);
 
     // Send data to API endpoint
+    setTimeout(() => {
+      console.log("Form submitted");
+      // Reenable Form Submission
+      setIsSubmitting(false);
+    }, 1000)
 
-    // Reenable Form Submission
-    setIsSubmitting(false);
-    setSent(true);
+
   };
 
   return (
-    <section className="h-screen bg-gradient-to-b from-background via-background to-primary/5 flex items-center justify-center p-4">
+    <section className="h-[calc(100vh_-_4rem)] bg-gradient-to-b from-background via-background to-primary/5 flex items-center justify-center p-4">
       <BlurFade delay={0.1}>
         <div className="w-full max-w-lg">
           {/* Header */}
@@ -114,9 +117,6 @@ const Signup = ({
                 }
                 // Otherwise, continue
                 form.handleSubmit(onFormSubmit)(e);
-              }}
-              onChange={() => {
-                setSent(false);
               }}>
 
 
